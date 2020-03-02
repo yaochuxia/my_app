a0f2a3de8a5295cd7d62e0e20bc2c1862259e5cd
yaochuxia <675993729@qq.com>
2020-03-02 22:34:49 +0800

    添加底部导航

--- src/navigators/AppNavigators.js
+++ src/navigators/AppNavigators.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator,createMaterialTopTabNavigator } from 'react-navigation-tabs';
-import { createDrawerNavigator,DrawerNavigatorItems }from 'react-navigation-drawer';
-import { SafeAreaView }from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { Button, Text, ScrollView } from 'react-native';
 import HomePage from '../pages/HomePage';
 import WelcomePage from '../pages/welcomePage';
 
