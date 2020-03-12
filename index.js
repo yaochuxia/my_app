7f5f341b9632ff01648782fe7f02d87e2ab24d51
yaochuxia <675993729@qq.com>
2020-03-12 22:39:59 +0800

    edit

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-// import AppStackNavigator from './src/navigators/AppNavigators';
 
 
 AppRegistry.registerComponent(appName, () => App);
